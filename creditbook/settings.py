756c587f7daab3ea20f5efd4e19af9304389e46b
krRaunit <raunitkumar10apr@gmail.com>
2025-02-02 19:08:46 +0530

    Add venv to .gitignore and remove it from version control

--- creditbook/settings.py
+++ creditbook/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%1g9)x5g)b1=+kip&pqx7wh#py-k#@twn)_t-=9jhfdjo2&l$2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['creditbook.onrender.com', 'localhost']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
